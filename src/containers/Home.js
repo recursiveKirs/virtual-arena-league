9ab68f11c3b8f8f93d71bb952c7b27039f19c525
Jason Zhuo <jzhuo@umass.edu>
2018-10-13 12:42:56 -0400

    adding features to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,6 @@ export default class Home extends Component {
       <div className="Home">
         <div className="lander">
           <h1>Virtual Arena League</h1>
-          <p>A simple note taking app</p>
         </div>
       </div>
     );
