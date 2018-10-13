e7903538cd4825160cc7a3dde236d3cd7963d45f
Jason Zhuo <jzhuo@umass.edu>
2018-10-13 01:39:47 -0400

    added alt text to logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo"/>
+          <img src={logo} className="App-logo" alt="react-logo"/>
           <p>
             Virtual Arena League
           </p>
