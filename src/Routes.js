7497aa8ee640003e2874d2cad191194af0afbe9e
Kirs Imsong <kirs.l.imsong@gmail.com>
2018-10-13 05:36:57 -0400

    Add Login page to handle Login Button. Add NotFound page to handle 404 Not Found messages.

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    { /* Finally, catch all unmatched routes */ }
+    <Route path="/login" exact component={Login} />
+    <Route component={NotFound} />
   </Switch>;
