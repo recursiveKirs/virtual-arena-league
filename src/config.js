9220f4afb82c1082637563aeb61887f225aab462
Jason Zhuo <jzhuo@umass.edu>
2018-10-14 05:39:06 -0400

    Update config file

--- src/config.js
+++ src/config.js
@@ -8,19 +8,19 @@ export default {
       //   Chapter "Deploy the APIs": https://serverless-stack.com/chapters/deploy-the-apis.html
       //   Example URL: https://ly55wbovq4.execute-api.us-east-
       //   EXAMPLE_API_GATEWAY_REGION = us-east-1
-      REGION: "us-east-2",
+      REGION: "us-east-1",
       //   EXAMPLE_API_GATEWAY_URL = 1.amazonaws.com/prod
-      URL: "https://lhykklvy97.execute-api.us-east-2.amazonaws.com/prodL"
+      URL: "https://9tbk8etr81.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
       // EXAMPLE_COGNITO_REGION = Chapter "Create a Cognito identity pool": Create a Cognito identity pool
-      REGION: "us-east-2",
+      REGION: "us-east-1",
       // EXAMPLE_COGNITO_USER_POOL_ID = "Pool Id"
-      USER_POOL_ID: "us-east-2_UqtG9UA7P",
+      USER_POOL_ID: "us-east-1_r6V6fOKb4",
       // EXAMPLE_COGNITO_APP_CLIENT_ID = "App Client id"
-      APP_CLIENT_ID: "1ittcqp2d4m072174moppfpnmr",
+      APP_CLIENT_ID: "4r8fl84hndtifvfaiif3lo1j92",
       // EXAMPLE_IDENTITY_POOL_ID = Chapter "Create a Cognito identity pool": https://serverless-stack.com/chapters/create-a-cognito-identity-pool.html
-      IDENTITY_POOL_ID: "us-east-2:81f8f75e-030a-4a2c-8c0c-fae68707aede"
+      IDENTITY_POOL_ID: "us-east-1:a5259ea4-81a6-4dc4-9a97-add93d2f07b0"
     }
   };
 
