f018f01ae63923a533885563f41df01fdf6b161b
Kirs Imsong <kirs.l.imsong@gmail.com>
2018-10-14 07:25:38 -0400

    Add links to carousel

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -12,38 +12,51 @@ export default class Home extends Component {
           <p>A platform for discovering and organizing competitive eSports Tournaments for everyone.</p>
           <Carousel id="home_carousel">
             <Carousel.Item>
+              <a href="https://www.epicgames.com/fortnite/en-US/fall-skirmish">
               <div id="carousel_image1">
               </div>
+              </a>
             </Carousel.Item>
+            
             <Carousel.Item>
+              <a href="https://www.lolesports.com/en_US/worlds">
               <div id="carousel_image2">
               </div>
+              </a>
             </Carousel.Item>
+            
             <Carousel.Item>
+              <a href="https://pro.eslgaming.com/csgo/proleague/">
               <div id="carousel_image3">
               </div>
+              </a>
             </Carousel.Item>
           </Carousel>
  
-          <LinkContainer
-                  key="new"
-                  to="/Fortnite"
-                >
+          <LinkContainer key="new" to="/Fortnite">
           <Jumbotron id="fortnite_jumbotron" >
           </Jumbotron>
           </LinkContainer>
 
+          <LinkContainer key="new" to="/Fortnite">
           <Jumbotron id="league_jumbotron">
           </Jumbotron>
+          </LinkContainer>
 
+          <LinkContainer key="new" to="/Fortnite">
           <Jumbotron id="starcraft2_jumbotron">
           </Jumbotron>
+          </LinkContainer>
 
+          <LinkContainer key="new" to="/Fortnite">
           <Jumbotron id="dota2_jumbotron">
           </Jumbotron>
-          
+          </LinkContainer>
+
+          <LinkContainer key="new" to="/Fortnite">
           <Jumbotron id="csgo_jumbotron">
           </Jumbotron>
+          </LinkContainer>
         </div>
       </div>
     );
